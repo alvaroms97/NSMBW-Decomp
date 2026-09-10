7d5ee9090f00dabce3ea85944cc93b2b8e99a31c
alvaroms97 <almorsan_97@hotmail.com>
2026-09-10 14:31:26 +0200

    Decompile `DVDDiskCheck`

---
+++ source/lib/revolution/DVD/dvd.cpp
@@ -0,0 +1,6 @@
+// TENTATIVE NAME (dtk auto-generated): unresolved low-level DVD disk-check routine.
+extern "C" void DVDCheckDiskBca(); ///< @unofficial
+
+extern "C" void DVDDiskCheck() {
+    DVDCheckDiskBca();
+}
