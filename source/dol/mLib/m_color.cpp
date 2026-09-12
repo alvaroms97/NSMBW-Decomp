2b93e018d67c62adca93d88fdb69bf0a29fd041b
CLF78 <29682335+CLF78@users.noreply.github.com>
2026-09-13 00:02:19 +0200

    Apply code review

--- source/dol/mLib/m_color.cpp
+++ source/dol/mLib/m_color.cpp
@@ -1,9 +1,9 @@
 #include <game/mLib/m_color.hpp>
 
-void mColor::lerp(const GXColor &from, const GXColor &to, float t) {
+void mColor::lerp(const GXColor &c1, const GXColor &c2, float t) {
     float s = 1.0f - t;
-    r = from.r * s + to.r * t;
-    g = from.g * s + to.g * t;
-    b = from.b * s + to.b * t;
-    a = from.a * s + to.a * t;
+    r = c1.r * s + c2.r * t;
+    g = c1.g * s + c2.g * t;
+    b = c1.b * s + c2.b * t;
+    a = c1.a * s + c2.a * t;
 }
